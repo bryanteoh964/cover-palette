ffba30ebf55b4e928e8a2425f8621b9b35c0ea37
bteoh964 <68241796+bteoh964@users.noreply.github.com>
2024-09-09 16:11:02 -0400

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Cover Pallette",
+  description: "Book cover cover pallette generator",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
